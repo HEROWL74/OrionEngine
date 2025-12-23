93c7a1fb4770e875970d9fa1c70eeff1040eccfc
HEROWL <hiroakitogawa74@gmail.com>
2025-12-24 01:00:07 +0900

    Ref: Render-system in Renderer.cpp

--- src/Graphics/Renderer.cpp
+++ src/Graphics/Renderer.cpp
@@ -4,15 +4,33 @@ namespace Engine::Graphics
 {
 	void Renderer::beginFrame()
 	{
-
-	}
-	void Renderer::endFrame()
-	{
+		/*
+		TODO:
+		// - Command Allocater Reset
+		// - CommandList Reset
+		// - BackBuffer ‚Ì ResourceBarrier(Present -> RenderTarget)
+		*/
 
 	}
 
 	void Renderer::render(Scene& scene, Camera& camera)
 	{
+		// TODO À‘Ì‚ğ‚½‚¹‚é
+		ID3D12GraphicsCommandList* commandList = nullptr;
+		UINT frameIndex = 0;
 
+		scene.render(commandList, camera, frameIndex);
+	}
+
+
+	void Renderer::endFrame()
+	{
+		/*
+		TODO:
+		// - CommandList Close
+		// - ExecuteCommandLists
+		// - SwapChain Present
+		// - Fence
+		*/
 	}
 }
--- src/Graphics/Renderer.hpp
+++ src/Graphics/Renderer.hpp
@@ -2,11 +2,14 @@
 #include <wrl.h>
 #include <d3d12.h>
 #include <array>
+#include "Scene.hpp"
 
 namespace Engine::Graphics
 {
 	class Camera;
 	class Scene;
+
+	using Microsoft::WRL::ComPtr;
 
 	class Renderer
 	{
@@ -19,5 +22,7 @@ namespace Engine::Graphics
 		void endFrame();
 
 		void render(Scene& scene, Camera& camera);
+	private:
+		Scene scene;
 	};
 }
